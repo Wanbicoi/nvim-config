2d687fa590694df56b0e5b06cd5cf531630fd4ef
Pi ne <wanbicoi123@gmail.com>
2023-11-07 14:40:04 +0700

    add wakatime, window auto resize

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -183,9 +183,7 @@ return {
       { "S", false},
     },
   },
-  {
-    "wakatime/vim-wakatime",
-  },
+  { "wakatime/vim-wakatime" },
   {
     "hrsh7th/nvim-cmp",
     dependencies = { "hrsh7th/cmp-nvim-lsp-signature-help" },
@@ -195,4 +193,7 @@ return {
       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "nvim_lsp_signature_help" } }))
     end,
   },
+  { "nvim-focus/focus.nvim", opts = {} },
+  { "j-hui/fidget.nvim", tag = "legacy", event = "LspAttach", opts = {} },
+  { "hinell/lsp-timeout.nvim", event = "LspAttach" },
 }

b029a5c7d323157d21528bbcdc996ec17bebe665
TrungHoang <trung.hoang@angi.com>
2025-01-11 09:52:04 +0700

    int

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,7 +12,15 @@ return {
       require "configs.lspconfig"
     end,
   },
-
+{
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+}
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {

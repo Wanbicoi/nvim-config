c012ac7a569957a273a8bb1a52fcfd332604bd9d
Trung Hoang <wanbicoi123@gmail.com>
2025-05-10 13:13:17 +0700

    clean up

--- init.lua
+++ init.lua
@@ -152,71 +152,6 @@ vim.o.winminwidth = 20
 --   ]]
 -- end
 
-vim.g.gutentags_file_list_command = 'rg --files'
-vim.g.gutentags_cache_dir = vim.fn.stdpath 'cache' .. '/vim-gutentags'
---vim.g.gutentags_trace = true
-vim.g.gutentags_ctags_exclude = {
-  '.git',
-  '.hg',
-  '.svn',
-  '.pijul',
-  '_darcs',
-  'node_modules',
-  'vendor',
-  'venv',
-  '__pypackages__',
-  'packages',
-  'Pods',
-  'target',
-  'build',
-  'dist',
-  'libs',
-  'out',
-  'bin',
-  'obj',
-  '*.o',
-  '*.exe',
-  '*.dll',
-  '*.so',
-  '*.dylib',
-  '*.pyc',
-  '__pycache__',
-  '*.class',
-  '*.jar',
-  '*.war',
-  '*.ear',
-  '.next',
-  'coverage',
-  '*.log',
-  '*.min.js',
-  '*.min.css',
-  '*.map',
-  '*.lock',
-  '*.json',
-  '*.xml',
-  '*.pdf',
-  '*.doc',
-  '*.docx',
-  '*.svg',
-  '*.png',
-  '*.jpg',
-  '*.jpeg',
-  '*.gif',
-  '*.ico',
-  '*.swp',
-  '*.swo',
-  '*.bak',
-  '*.tmp',
-  '*.cache',
-  '.DS_Store',
-  '*.db',
-  '*.yaml',
-  '*.yml',
-  '*.suo',
-  '*.user',
-  '*.sln',
-}
-
 -- 🆙 Support ascx filetype
 vim.filetype.add { extension = { ascx = 'html' } }
 vim.treesitter.language.register('html', 'ascx')
@@ -495,90 +430,26 @@ require('lazy').setup({
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for Neovim
-      -- Mason must be loaded before its dependents so we need to set it up here.
-      -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
       { 'williamboman/mason.nvim', opts = {} },
       { 'williamboman/mason-lspconfig.nvim' },
       -- 'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
-
-      -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',
     },
     config = function()
-      -- Brief aside: **What is LSP?**
-      --
-      -- LSP is an initialism you've probably heard, but might not understand what it is.
-      --
-      -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-      -- and language tooling communicate in a standardized fashion.
-      --
-      -- In general, you have a "server" which is some tool built to understand a particular
-      -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-      -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-      -- processes that communicate with some "client" - in this case, Neovim!
-      --
-      -- LSP provides Neovim with features like:
-      --  - Go to definition
-      --  - Find references
-      --  - Autocompletion
-      --  - Symbol Search
-      --  - and more!
-      --
-      -- Thus, Language Servers are external tools that must be installed separately from
-      -- Neovim. This is where `mason` and related plugins come into play.
-      --
-      -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-      -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
-      --  This function gets run when an LSP attaches to a particular buffer.
-      --    That is to say, every time a new file is opened that is associated with
-      --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-      --    function will be executed to configure the current buffer
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
-          -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-          -- to define small helper and utility functions so you don't have to repeat yourself.
-          --
-          -- In this case, we create a function that lets us more easily define mappings specific
-          -- for LSP related items. It sets the mode, buffer and description for us each time.
           local map = function(keys, func, desc, mode)
             mode = mode or 'n'
             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
-          map('<leader>ds', vim.diagnostic.open_float, '[D]iagno[s]tic open float')
-          -- Jump to the definition of the word under your cursor.
-          --  This is where a variable was first declared, or where a function is defined, etc.
-          --  To jump back, press <C-t>.
+          map('gh', vim.diagnostic.open_float, '[D]iagno[s]tic open float')
           map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-
-          -- Find references for the word under your cursor.
           map('gR', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-          -- Jump to the implementation of the word under your cursor.
-          --  Useful when your language has ways of declaring types without an actual implementation.
-          map('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-
-          -- Jump to the type of the word under your cursor.
-          --  Useful when you're not sure what type a variable is and you want to see
-          --  the definition of its *type*, not where it was *defined*.
-          -- map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-
-          -- Fuzzy find all the symbols in your current document.
-          --  Symbols are things like variables, functions, types, etc.
-          -- map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-
-          -- Fuzzy find all the symbols in your current workspace.
-          --  Similar to document symbols, except searches over your entire project.
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-          -- WARN: This is not Goto Definition, this is Goto Declaration.
-          --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
           -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
@@ -962,7 +833,13 @@ require('lazy').setup({
       --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
+      require('mini.ai').setup {
+        n_lines = 500,
+        mappings = {
+          goto_left = '',
+          goto_right = '',
+        },
+      }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
@@ -971,16 +848,13 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup {
         mappings = {
-          add = 'gsa', -- Add surrounding in Normal and Visual modes
-          delete = 'gsd', -- Delete surrounding
-          find = 'gsf', -- Find surrounding (to the right)
-          find_left = 'gsF', -- Find surrounding (to the left)
+          add = 'gsa',
+          delete = 'gsd',
+          find = 'gsf',
+          find_left = 'gsF',
           highlight = 'gsh', -- Highlight surrounding
           replace = 'gsr', -- Replace surrounding
           update_n_lines = 'gsn', -- Update `n_lines`
-
-          suffix_last = 'l', -- Suffix to search with "prev" method
-          suffix_next = 'n', -- Suffix to search with "next" method
         },
         n_lines = 1000,
       }
@@ -1347,6 +1221,72 @@ require('lazy').setup({
   {
     'ludovicchabant/vim-gutentags',
     event = 'VeryLazy',
+    config = function()
+      vim.g.gutentags_file_list_command = 'rg --files'
+      vim.g.gutentags_cache_dir = vim.fn.stdpath 'cache' .. '/vim-gutentags'
+      --vim.g.gutentags_trace = true
+      vim.g.gutentags_ctags_exclude = {
+        '.git',
+        '.hg',
+        '.svn',
+        '.pijul',
+        '_darcs',
+        'node_modules',
+        'vendor',
+        'venv',
+        '__pypackages__',
+        'packages',
+        'Pods',
+        'target',
+        'build',
+        'dist',
+        'libs',
+        'out',
+        'bin',
+        'obj',
+        '*.o',
+        '*.exe',
+        '*.dll',
+        '*.so',
+        '*.dylib',
+        '*.pyc',
+        '__pycache__',
+        '*.class',
+        '*.jar',
+        '*.war',
+        '*.ear',
+        '.next',
+        'coverage',
+        '*.log',
+        '*.min.js',
+        '*.min.css',
+        '*.map',
+        '*.lock',
+        '*.json',
+        '*.xml',
+        '*.pdf',
+        '*.doc',
+        '*.docx',
+        '*.svg',
+        '*.png',
+        '*.jpg',
+        '*.jpeg',
+        '*.gif',
+        '*.ico',
+        '*.swp',
+        '*.swo',
+        '*.bak',
+        '*.tmp',
+        '*.cache',
+        '.DS_Store',
+        '*.db',
+        '*.yaml',
+        '*.yml',
+        '*.suo',
+        '*.user',
+        '*.sln',
+      }
+    end,
   },
   {
     'tpope/vim-fugitive',
@@ -1363,7 +1303,7 @@ require('lazy').setup({
   --
   -- require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
-  require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.indent_line',
